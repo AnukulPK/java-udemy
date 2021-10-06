6a5076b17bdbada81779c55f54ac309ac2012a70
AnukulPK <anukulpk2@gmail.com>
2021-10-06 22:47:51 +0530

    added operators

---
+++ Operators/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int result = 1+2;
+        System.out.println("1+2 = "+result);
+        int previousResult = result;
+        System.out.println("previousResult= "+previousResult);
+        result = result -1;
+        System.out.println("result= "+result);
+        result = result%2;
+        System.out.println("result%= "+result);
+
+    }
+}
