a0674038dc88a150fad272f1fda9b84c7253781e
Anukul <anukul.p@growthsourceft.com>
2021-10-21 10:11:53 +0530

    exercise attempted

--- Strings/src/com/company/Main.java
+++ Strings/src/com/company/Main.java
@@ -4,9 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+//
+//        String myString = "This is a string";
+//        System.out.println("My string = "+myString);
 
-        String myString = "This is a string";
-        System.out.println("My string = "+myString);
+        double value1 = 20.00;
+        double value2 = 80.00;
+        double sol1 = (value1+value2)*100.00d;
+        double remainder = sol1 % 40.00d;
+
+        boolean answer ;
+        if(remainder==0){
+            answer = true;
+        }else{
+            answer = false;
+        }
+
+        System.out.println(answer);
+
+        if(!answer ){
+            System.out.println("Got some remainder");
+        }
+
 
 
     }
