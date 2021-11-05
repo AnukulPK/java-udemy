19c351534edb99ffef401b27f48fada5708f06f9
Anukul <anukul.p@growthsourceft.com>
2021-11-05 10:32:37 +0530

    method overloading

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -8,8 +8,16 @@ public class Hello {
         int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
         int myLastOne = 1000-myTotal;
         System.out.println(myLastOne);
+    }
 
-
+    public static boolean isLeapYear(int year){
+        if(year>=1 && year<=9999){
+            if(year%4==0 && (year%100==0) && (year%400==0)){
+                return true;
+            }else{
+                return false;
+            }
+        }else return false;
 
     }
 }
