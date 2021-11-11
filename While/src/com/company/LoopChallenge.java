59c64efc89d619c3bc79dd658c3f2d6d8370405e
Anukul <anukul.p@growthsourceft.com>
2021-11-11 16:24:01 +0530

    while challenge added

---
+++ While/src/com/company/LoopChallenge.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class LoopChallenge {
+    public static void main(String[] args) {
+        System.out.println(sumDigits(125));
+    }
+
+    public static int sumDigits(int number){
+        if(number<=10){
+            return -1;
+        }
+
+        int sum = 0;
+
+        while(number%10!=0){
+            sum += number%10;
+            number = number/10;
+//            System.out.println("number = " + number);
+//            System.out.println("sum = " + sum);
+        }
+
+        return sum;
+    }
+}
--- While/src/com/company/Main.java
+++ While/src/com/company/Main.java
@@ -4,5 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+//        int count = 1;
+//        while(count!=5){
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+
+        int number = 4;
+        int finishNumber = 20;
+        int countOfEvenNumbers = 0;
+
+        while(number<=finishNumber){
+            number++;
+
+            if(!isEvenNumber(number)){
+                continue;
+            }
+            System.out.println("Even number " + number);
+            countOfEvenNumbers++;
+
+            if(countOfEvenNumbers>=5){
+                break;
+            }
+
+
+        }
+
+        System.out.println("Total number of even numbers found "+countOfEvenNumbers);
+    }
+
+    public static boolean isEvenNumber(int number){
+        if(number%2==0){
+            return true;
+        }else return  false;
     }
 }
