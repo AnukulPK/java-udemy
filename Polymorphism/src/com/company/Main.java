48f8d172b51fced2fcd44a48f4fdf1e29a4602a1
Anukul <anukul.p@growthsourceft.com>
2021-12-16 10:07:40 +0530

    polymorphism implemeneted

--- Polymorphism/src/com/company/Main.java
+++ Polymorphism/src/com/company/Main.java
@@ -1,14 +1,19 @@
 package com.company;
 
 class Movie{
-    private String movie;
+    private String name;
 
     public Movie(String movie) {
-        this.movie = movie;
+        this.name = movie;
     }
 
     public String plot(){
         return "No Plot Here";
+    }
+
+    public String getName() {
+        return name;
+
     }
 }
 
@@ -68,7 +73,32 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        for(int i = 1;i<11;i++){
+            Movie movie = randomMovie();
+            System.out.println("Movie # " + i +" : " + movie.getName()+"\n"+
+                    "Plot " + movie.plot()+"\n" );
 
+        }
+
+    }
+
+    public static Movie randomMovie(){
+        int randomNumber =(int) (Math.random() * 5)+1;
+
+        System.out.println("Random number generated was : " +randomNumber);
+
+        switch (randomNumber){
+            case 1: return new Jaws();
+
+            case 2: return new IndependenceDay();
+
+            case 3: return new MazeRunner();
+
+            case 4: return new StarWars();
+
+            case 5: return new ForgetableMovie();
+        }
+        return null;
     }
 }
 
