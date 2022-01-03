c4288fadbe32933449c34ed1e91ff2770673d7ae
AnukulPK <anukulpk2@gmail.com>
2022-01-03 23:08:22 +0530

    polymorphism challenge completed

---
+++ Polymorphism-challenge/src/com/company/Mitsubishi.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Mitsubishi extends Car{
+
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi->Start engine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi->accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi->brake()";
+    }
+}
