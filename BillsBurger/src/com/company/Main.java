4dcbeed7260f6d87fb7d775515f08d057bd881a6
AnukulPK <anukulpk2@gmail.com>
2022-01-05 23:23:36 +0530

    Bills Burger challenge

---
+++ BillsBurger/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Hamburger hamburger = new Hamburger("Basic","Sausage",3.56,"White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato",0.27);
+        hamburger.addHamburgerAddition2("Lettuce",0.75);
+        hamburger.addHamburgerAddition3("Cheese",1.12);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon",5.67);
+
+        healthyBurger.addHamburgerAddition1("Egg",5.43);
+
+        healthyBurger.addHealthAddition1("Lentils",3.41);
+        healthyBurger.itemizeHamburger();
+        System.out.println("Total Healthy Burger price is " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+
+        db.addHamburgerAddition3("Nope",50.53);
+        db.itemizeHamburger();
+    }
+}
