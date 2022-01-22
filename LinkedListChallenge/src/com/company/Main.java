2b981d4237c340c9b6a1643b6f9f8c1ccfc77939
AnukulPK <anukulpk2@gmail.com>
2022-01-22 13:17:35 +0530

    linkedlist challenge completely

--- LinkedListChallenge/src/com/company/Main.java
+++ LinkedListChallenge/src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -59,6 +56,7 @@ public class Main {
             return;
         }else{
             System.out.println("Now playing "+ listIterator.next().toString());
+            printMenu();
         }
         while(!quit){
             int action = scanner.nextInt();
@@ -96,16 +94,54 @@ public class Main {
                         forward=true;
                     }
                     break;
-                case 3: break;
+                case 3:
+                    if(forward){
+                        if(listIterator.hasPrevious()){
+                            System.out.println("Now Replaying " + listIterator.previous().toString());
+                            forward=false;
+                        }else{
+                            System.out.println("We are at the start of the list");
+                        }
+                    }else {
+                        if(listIterator.hasNext()){
+                            System.out.println("Now replaying "+listIterator.next().toString());
+                            forward=true;
+                        }else{
+                            System.out.println("we have reached the end of the list");
+                        }
+                    }
+                    break;
                 case 4:
-//                    printList(playList);
+                    printList(playList);
                     break;
                 case 5:
-//                    printMenu();
+                    printMenu();
                     break;
-
-
+                case 6: if(playList.size()>0){
+                    listIterator.remove();
+                    if(listIterator.hasNext()){
+                        System.out.println("Now playing "+listIterator.next());
+                    }else if(listIterator.hasPrevious()){
+                        System.out.println("Now playing "+listIterator.previous());
+                    }
+                }
+                break;
             }
         }
     }
+
+    private static void printMenu(){
+        System.out.println("Available actions");
+    }
+
+    private static void printList(LinkedList<Song> playList){
+        Iterator<Song> iterator = playList.iterator();
+        System.out.println("===================");
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println("====================");
+    }
+    
+
 }
