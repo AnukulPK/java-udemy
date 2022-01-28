ffeee6d12a277f6d7d2578fdfb4365d9a87a187c
Anukul <anukul.p@growthsourceft.com>
2022-01-28 12:40:31 +0530

    inner class wip

---
+++ InnerClasses/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Gearbox mclaren = new Gearbox(6);
+//        Gearbox.Gear first = mclaren.new Gear(1,12.3);
+//        System.out.println(first.driveSpeed(1000));
+    }
+}
