6c4a1d6252386e99e64baf7aced26b07171dffc6
unknown <anukulpk2@gmail.com>
2022-07-29 14:24:32 +0530

    generics part 2

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -2,14 +2,24 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> items = new ArrayList<>();
-        items.add(1);
-        items.add(2);
-        items.add(3);
-        items.add(4);
-        items.add(5);
+//        ArrayList<Integer> items = new ArrayList<>();
+//        items.add(1);
+//        items.add(2);
+//        items.add(3);
+//        items.add(4);
+//        items.add(5);
+//
+//        printDoubled(items);
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+        Team csk = new Team("CSK");
+        csk.addPlayer(joe);
+        csk.addPlayer(pat);
+        csk.addPlayer(beckham);
 
-        printDoubled(items);
+        System.out.println(csk.numPlayers());
+
     }
 
     private static void printDoubled(ArrayList<Integer> n){
