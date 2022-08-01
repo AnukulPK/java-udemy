896bf46033d5fadbe52b242a29b809cd8f924736
unknown <anukulpk2@gmail.com>
2022-08-01 17:06:35 +0530

    generics part 2 added

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -1,30 +1,38 @@
-import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
-//        ArrayList<Integer> items = new ArrayList<>();
-//        items.add(1);
-//        items.add(2);
-//        items.add(3);
-//        items.add(4);
-//        items.add(5);
-//
-//        printDoubled(items);
+
         FootballPlayer joe = new FootballPlayer("Joe");
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer beckham = new SoccerPlayer("Beckham");
-        Team csk = new Team("CSK");
+        Team<FootballPlayer> csk = new Team<>("CSK");
         csk.addPlayer(joe);
-        csk.addPlayer(pat);
-        csk.addPlayer(beckham);
 
         System.out.println(csk.numPlayers());
 
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Bulls");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this wont work");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourbe = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+        melbourbe.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorne = new Team<>("Hawthorne");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorne.matchResult(fremantle,1,0);
+
+
+
     }
 
-    private static void printDoubled(ArrayList<Integer> n){
-        for(int i :n) {
-            System.out.println(i * 2);
-        }
-    }
+//    private static void printDoubled(ArrayList<Integer> n){
+//        for(int i :n) {
+//            System.out.println(i * 2);
+//        }
+//    }
 }
