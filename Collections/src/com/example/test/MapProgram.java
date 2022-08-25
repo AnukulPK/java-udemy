88318d02cecc2a0a8b0853280d31d9a2c8f975da
unknown <anukulpk2@gmail.com>
2022-08-25 10:38:28 +0530

    maps added

--- Collections/src/com/example/test/MapProgram.java
+++ Collections/src/com/example/test/MapProgram.java
@@ -21,6 +21,20 @@ public class MapProgram {
 //        System.out.println((languages.get("Java")));
         System.out.println("===========================================");
 
+//        languages.remove("Lisp");
+        if(languages.remove("Algol","a family of algorthmic languages")){
+            System.out.println("Algol removed");
+        }else{
+            System.out.println("Algol not removed, key value paid not found");
+        }
+
+
+       if(languages.replace("lisp","Thsi will not work","This will work")){
+           System.out.println("Lisp");
+       } else{
+           System.out.println("Lisp was not replaced");
+       }
+        System.out.println(languages.replace("Scala","This will not be added"));
         for(String key: languages.keySet()){
             System.out.println(key+" : "+languages.get(key));
         }
