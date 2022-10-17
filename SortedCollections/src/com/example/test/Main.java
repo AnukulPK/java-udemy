362e0398a62420e8b9fb3739ab408fffe086265d
unknown <anukulpk2@gmail.com>
2022-10-17 16:21:47 +0530

    adding to basket

--- SortedCollections/src/com/example/test/Main.java
+++ SortedCollections/src/com/example/test/Main.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import java.util.Map;
+
 public class Main {
     private static StockList stockList = new StockList();
 
@@ -15,6 +17,9 @@ public class Main {
         stockList.addStock(temp);
 
         temp = new StockItem("Chair",0.50,200);
+        stockList.addStock(temp);
+
+        temp = new StockItem("Cup",0.45,7);
         stockList.addStock(temp);
 
         temp = new StockItem("Cup",62.00,10);
@@ -41,5 +46,52 @@ public class Main {
             System.out.println(s);
         }
 
+        Basket anukulsBasket = new Basket("Anukul");
+        sellItem(anukulsBasket,"Car",1);
+        System.out.println(anukulsBasket);
+
+        sellItem(anukulsBasket,"Car",1);
+        System.out.println(anukulsBasket);
+
+        if(sellItem(anukulsBasket,"Car",1)!=1){
+            System.out.println("There are no more cars in stock");
+        }
+
+//        sellItem(anukulsBasket,"Car",1);
+        sellItem(anukulsBasket,"Spanner",5);
+        System.out.println(anukulsBasket);
+
+        sellItem(anukulsBasket,"Juice",4);
+        sellItem(anukulsBasket,"Cup",12);
+        sellItem(anukulsBasket,"Bread",1);
+        System.out.println(anukulsBasket);
+        System.out.println(stockList);
+
+//        temp = new StockItem("Pen",1,12);
+//        stockList.Items().put(temp.getName(), temp);
+
+        stockList.Items().get("Car").adjustStock(2000);
+        stockList.get("Car").adjustStock(-1000);
+        System.out.println(stockList);
+        for(Map.Entry<String,Double> price:stockList.PriceList().entrySet()){
+            System.out.println(price.getKey()+" costs "+price.getValue());
+        }
+
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity){
+        //retrieve item from stock list
+        StockItem stockItem = stockList.get(item);
+        if(stockItem==null){
+            System.out.println("We don't sell "+item);
+            return 0;
+        }
+
+        if(stockList.sellStock(item,quantity)!=0){
+            basket.addToBasket(stockItem,quantity);
+            return quantity;
+        }
+
+        return 0;
     }
 }
--- SortedCollections/src/com/example/test/StockList.java
+++ SortedCollections/src/com/example/test/StockList.java
@@ -46,6 +46,15 @@ public class StockList {
         return Collections.unmodifiableMap(list);
     }
 
+    public Map<String, Double> PriceList(){
+        Map<String, Double> prices = new LinkedHashMap<>();
+        for(Map.Entry<String,StockItem> item:list.entrySet()){
+         prices.put(item.getKey(),item.getValue().getPrice());
+        }
+
+        return Collections.unmodifiableMap(prices);
+    }
+
     @Override
     public String toString() {
         String s = "\nStock List\n";
@@ -61,6 +70,6 @@ public class StockList {
             totalCost+=itemValue;
 
         }
-        return s+"Total stock Value "+String.format("%.2f",totalCost);
+        return s+" Total stock Value "+String.format("%.2f",totalCost);
     }
 }
