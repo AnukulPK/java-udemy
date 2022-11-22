3e4719476dbb869d429f10162c357e5fe73306d4
unknown <anukulpk2@gmail.com>
2022-11-22 10:04:40 +0530

    event and event handlers

---
+++ Events/src/main/java/com/example/events/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.events;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+  @FXML
+  private TextField nameField;
+
+  public void onButtonClicked(){
+      System.out.println("Hello, "+nameField.getText());
+  }
+}
