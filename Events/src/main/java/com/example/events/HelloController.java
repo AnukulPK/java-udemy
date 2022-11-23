ded612a5c18ded695f4e9c474c79d6bd2261a0c7
unknown <anukulpk2@gmail.com>
2022-11-23 10:29:20 +0530

    events continued

--- Events/src/main/java/com/example/events/HelloController.java
+++ Events/src/main/java/com/example/events/HelloController.java
@@ -1,14 +1,38 @@
 package com.example.events;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class HelloController {
-  @FXML
-  private TextField nameField;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
 
-  @FXML
-  public void onButtonClicked(){
-      System.out.println("Hello, "+nameField.getText());
-  }
+    @FXML
+    public void initialize(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent event) {
+        if (event.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if (event.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased(){
+        String text = nameField.getText();
+        boolean disableButton = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButton);
+        byeButton.setDisable(disableButton);
+    }
 }
