6edcf074b5c978f71a21b6cf5295c645021d5247
unknown <anukulpk2@gmail.com>
2022-12-06 11:21:08 +0530

    basic UI added

--- TodoList/src/main/java/com/example/todolist/HelloController.java
+++ TodoList/src/main/java/com/example/todolist/HelloController.java
@@ -1,14 +1,34 @@
 package com.example.todolist;
 
+import com.example.todolist.datamodel.TodoItem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController {
+
+    private List<TodoItem> todoItems;
+
+    public void initialize(){
+        TodoItem item1 = new TodoItem("Mail birthday card","Buy a 30th bithday card for John", LocalDate.of(2016, Month.APRIL, 25));
+        TodoItem item2 = new TodoItem("Doctors Appointment","See Dr.Smith at 123 Main street. Bring paperwork", LocalDate.of(2016, Month.MAY, 23));
+        TodoItem item3 = new TodoItem("Finish Design proposal","Send mockups to Mike by Friday", LocalDate.of(2016, Month.APRIL, 22));
+        TodoItem item4 = new TodoItem("Pickup Doug at train station","Doug's arriving on March 23 on the 5:00 train", LocalDate.of(2016, Month.MARCH, 23));
+        TodoItem item5 = new TodoItem("Pickup dry cleaning","The clothes should be ready", LocalDate.of(2016, Month.APRIL, 20));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+
+
     }
+
+
 }
