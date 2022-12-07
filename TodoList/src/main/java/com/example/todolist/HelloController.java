90abefbfd00f148772b1cf7612040ad7b98a2be0
unknown <anukulpk2@gmail.com>
2022-12-07 10:31:34 +0530

    text area added

--- TodoList/src/main/java/com/example/todolist/HelloController.java
+++ TodoList/src/main/java/com/example/todolist/HelloController.java
@@ -3,6 +3,9 @@ package com.example.todolist;
 import com.example.todolist.datamodel.TodoItem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -13,12 +16,17 @@ public class HelloController {
 
     private List<TodoItem> todoItems;
 
-    public void initialize(){
-        TodoItem item1 = new TodoItem("Mail birthday card","Buy a 30th bithday card for John", LocalDate.of(2016, Month.APRIL, 25));
-        TodoItem item2 = new TodoItem("Doctors Appointment","See Dr.Smith at 123 Main street. Bring paperwork", LocalDate.of(2016, Month.MAY, 23));
-        TodoItem item3 = new TodoItem("Finish Design proposal","Send mockups to Mike by Friday", LocalDate.of(2016, Month.APRIL, 22));
-        TodoItem item4 = new TodoItem("Pickup Doug at train station","Doug's arriving on March 23 on the 5:00 train", LocalDate.of(2016, Month.MARCH, 23));
-        TodoItem item5 = new TodoItem("Pickup dry cleaning","The clothes should be ready", LocalDate.of(2016, Month.APRIL, 20));
+    @FXML
+    private ListView<TodoItem> todoListView;
+    @FXML
+    private TextArea itemDetailsTextArea;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th bithday card for John", LocalDate.of(2016, Month.APRIL, 25));
+        TodoItem item2 = new TodoItem("Doctors Appointment", "See Dr.Smith at 123 Main street. Bring paperwork", LocalDate.of(2016, Month.MAY, 23));
+        TodoItem item3 = new TodoItem("Finish Design proposal", "Send mockups to Mike by Friday", LocalDate.of(2016, Month.APRIL, 22));
+        TodoItem item4 = new TodoItem("Pickup Doug at train station", "Doug's arriving on March 23 on the 5:00 train", LocalDate.of(2016, Month.MARCH, 23));
+        TodoItem item5 = new TodoItem("Pickup dry cleaning", "The clothes should be ready", LocalDate.of(2016, Month.APRIL, 20));
 
         todoItems = new ArrayList<TodoItem>();
         todoItems.add(item1);
@@ -26,6 +34,21 @@ public class HelloController {
         todoItems.add(item3);
         todoItems.add(item4);
         todoItems.add(item5);
+
+        todoListView.getItems().setAll(todoItems);
+
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+//        System.out.println("the selected item is "+item);
+        StringBuilder sb = new StringBuilder(item.getDetails());
+        sb.append("\n\n\n\n");
+        sb.append("Due: ");
+        sb.append(item.getDeadline().toString());
+        itemDetailsTextArea.setText(sb.toString());
 
 
     }
