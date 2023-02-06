da42a7db1ba596010e48b5a296f57a856682b142
unknown <anukulpk2@gmail.com>
2023-02-06 19:04:31 +0530

    file selection

--- JavaFX-Application/src/main/java/com/example/javafxapplication/HelloApplication.java
+++ JavaFX-Application/src/main/java/com/example/javafxapplication/HelloApplication.java
@@ -8,10 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+//        setUserAgentStylesheet(STYLESHEET_CASPIAN);
         Scene scene = new Scene(fxmlLoader.load(), 600, 275);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -19,6 +21,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
