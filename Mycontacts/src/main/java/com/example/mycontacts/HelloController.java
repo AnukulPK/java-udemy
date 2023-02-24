f4f61a35dd8ec07cdec76df1bd92bf80411ad5ad
unknown <anukulpk2@gmail.com>
2023-02-24 14:23:31 +0530

    adding a contact

--- Mycontacts/src/main/java/com/example/mycontacts/HelloController.java
+++ Mycontacts/src/main/java/com/example/mycontacts/HelloController.java
@@ -1,19 +1,59 @@
 package com.example.mycontacts;
 
+import com.example.mycontacts.datamodel.Contact;
+import com.example.mycontacts.datamodel.ContactData;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private BorderPane mainPanel;
+
+    @FXML
+    private TableView<Contact> contactsTable;
+
+    private ContactData data;
+
+    public void initialize(){
+        data=new ContactData();
+        data.loadContacts();
+        contactsTable.setItems(data.getContacts());
+    }
 
     @FXML
     public void showAddContactDialog(){
+        Dialog<ButtonType> dialog = new Dialog<ButtonType>();
+        dialog.initOwner(mainPanel.getScene().getWindow());
+        dialog.setTitle("Add New Contact");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("contactDialog.fxml"));
 
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        }catch (IOException e){
+            System.out.println("Could not load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get()==ButtonType.OK){
+            ContactController contactController = fxmlLoader.getController();
+            Contact newContact = contactController.getNewContact();
+            data.addContact(newContact);
+            data.saveContacts();
+        }
     };
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
--- Mycontacts/src/main/java/module-info.java
+++ Mycontacts/src/main/java/module-info.java
@@ -5,5 +5,6 @@ module com.example.mycontacts {
 
 
     opens com.example.mycontacts to javafx.fxml;
+    opens com.example.mycontacts.datamodel;
     exports com.example.mycontacts;
 }
