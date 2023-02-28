0814ec2d3681119c49e23af716070a47fbb09342
unknown <anukulpk2@gmail.com>
2023-02-28 10:56:18 +0530

    edit contact

--- Mycontacts/src/main/java/com/example/mycontacts/HelloController.java
+++ Mycontacts/src/main/java/com/example/mycontacts/HelloController.java
@@ -4,10 +4,7 @@ import com.example.mycontacts.datamodel.Contact;
 import com.example.mycontacts.datamodel.ContactData;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
@@ -55,5 +52,44 @@ public class HelloController {
             data.saveContacts();
         }
     };
+
+    @FXML
+    public void showEditContactDialog(){
+        Contact selectedContact = contactsTable.getSelectionModel().getSelectedItem();
+
+        if(selectedContact==null){
+            Alert alert = new Alert((Alert.AlertType.INFORMATION));
+            alert.setTitle("No Contact selected");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select the contact you want to edit");
+            alert.showAndWait();
+            return;
+        }
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainPanel.getScene().getWindow());
+        dialog.setTitle("Edit Contact");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("contactDialog.fxml"));
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        }catch(IOException e){
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        ContactController contactController=fxmlLoader.getController();
+        contactController.editContact(selectedContact);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get()==ButtonType.OK){
+            contactController.updateContact(selectedContact);
+            data.saveContacts();
+        }
+    }
 
 }
